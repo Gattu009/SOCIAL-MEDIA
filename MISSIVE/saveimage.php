efbe336680fd8be442b13d3cb75069060582ece2
Gattu009 <58887843+Gattu009@users.noreply.github.com>
2021-07-29 13:31:42 +0530

    Add files via upload

---
+++ MISSIVE/saveimage.php
@@ -0,0 +1,17 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$databasename="test1";
+$con=mysqli_connect($host,$user,$password,$databasename);
+$filename =  time() . '.jpg';
+$filepath = 'gallery/';
+if(!is_dir($filepath))
+	mkdir($filepath);
+if(isset($_FILES['webcam'])){	
+	move_uploaded_file($_FILES['webcam']['tmp_name'], $filepath.$filename);
+	$sql="Insert into camera(camera) values('$filename')";
+	$result=mysqli_query($con,$sql);
+	echo $filepath.$filename;
+}
+?>
